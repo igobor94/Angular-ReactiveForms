2c0c61c97b66e3e23520dd220d5315caabfeaaf6
Igor <igor.borowski@capgemini.com>
2022-07-20 17:14:14 +0200

    add reactive forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormComponent } from './form/form.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+
+
 
 @Component({
   selector: 'app-form',
@@ -7,9 +11,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormComponent implements OnInit {
 
-  constructor() { }
+  defaultForm: FormGroup | any;
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.defaultForm = this.fb.group({
+      firstName: ['', [Validators.required, Validators.minLength(3)]],
+      lastName: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      phone: '',
+      rating: null,
+      notification: 'text'
+    })
+  }
+
+  save() {
+    console.log(this.defaultForm)
+  }
+
+  setNotification(notifyVia: string): void {
+    const phoneControl = this.defaultForm.get('phone');
+
+    if (notifyVia === 'text') {
+      phoneControl.setValidators(Validators.required)
+    } else {
+      phoneControl.clearValidators();
+    }
+
+    phoneControl.updateValueAndValidity();
   }
 
 }

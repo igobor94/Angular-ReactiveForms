c67e09e69e43d33d679b7efa183c2024cbc0b912
Igor <igor.borowski@capgemini.com>
2022-07-20 17:31:25 +0200

    custom valdiator with parameters

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,12 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 
-
-function ratingRange(c: AbstractControl): { [key: string]: boolean } | null {
-  if (c.value !== null && (isNaN(c.value) || c.value < 1 || c.value > 5)) {
-    return { 'range': true }
+function ratingRange(min: number, max: number): ValidatorFn {
+  return (c: AbstractControl): { [key: string]: boolean } | null => {
+    if (c.value !== null && (isNaN(c.value) || c.value < 1 || c.value > 5)) {
+      return { 'range': true }
+    }
+    return null
   }
-  return null
 }
 
 @Component({
@@ -26,7 +27,7 @@ export class FormComponent implements OnInit {
       lastName: ['', [Validators.required, Validators.minLength(3)]],
       email: ['', [Validators.required, Validators.email]],
       phone: '',
-      rating: [null, ratingRange],
+      rating: [null, ratingRange(1, 5)],
       notification: 'text'
     })
   }

314ce90b411801ad5cfbc216f92054ff9233ef29
Igor <igor.borowski@capgemini.com>
2022-07-22 11:35:22 +0200

    setiings route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormComponent } from './form/form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: 'home', component: FormComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
